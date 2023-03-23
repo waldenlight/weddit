eca8c371f8e583f2560c938b5b1f8908ed6b9b6b
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-03-22 21:52:57 -0600

    Deploy

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'weddit.herokuapp.com',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
