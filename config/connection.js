7d0d9ddb5bb3ab712b5fa7c3c130df4d8eaeccb4
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-03-23 18:18:21 -0600

    Seeds

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,8 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'weddit.herokuapp.com',
+      // host: 'weddit.herokuapp.com',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
